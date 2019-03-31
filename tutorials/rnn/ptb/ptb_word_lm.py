321a3b3b854d9d8251937e12fd3527cc315a62e5
97chenxa <47140066+97chenxa@users.noreply.github.com>
2019-03-31 10:43:16 +0800

    Update ptb_word_lm.py

--- tutorials/rnn/ptb/ptb_word_lm.py
+++ tutorials/rnn/ptb/ptb_word_lm.py
@@ -436,91 +436,80 @@ def get_config():
     raise ValueError("Invalid model: %s", FLAGS.model)
   if FLAGS.rnn_mode:
     config.rnn_mode = FLAGS.rnn_mode
-  if FLAGS.num_gpus != 1 or tf.__version__ < "1.3.0" :
-    config.rnn_mode = BASIC
+
   return config
 
 
 def main(_):
-  if not FLAGS.data_path:
-    raise ValueError("Must set --data_path to PTB data directory")
-  gpus = [
-      x.name for x in device_lib.list_local_devices() if x.device_type == "GPU"
-  ]
-  if FLAGS.num_gpus > len(gpus):
-    raise ValueError(
-        "Your machine has only %d gpus "
-        "which is less than the requested --num_gpus=%d."
-        % (len(gpus), FLAGS.num_gpus))
+  with tf.device('/cpu:0'):
+    if not FLAGS.data_path:
+      raise ValueError("Must set --data_path to PTB data directory")
 
-  raw_data = reader.ptb_raw_data(FLAGS.data_path)
-  train_data, valid_data, test_data, _ = raw_data
 
-  config = get_config()
-  eval_config = get_config()
-  eval_config.batch_size = 1
-  eval_config.num_steps = 1
+    raw_data = reader.ptb_raw_data(FLAGS.data_path)
+    train_data, valid_data, test_data, _ = raw_data
 
-  with tf.Graph().as_default():
-    initializer = tf.random_uniform_initializer(-config.init_scale,
-                                                config.init_scale)
+    config = get_config()
+    eval_config = get_config()
+    eval_config.batch_size = 1
+    eval_config.num_steps = 1
 
-    with tf.name_scope("Train"):
-      train_input = PTBInput(config=config, data=train_data, name="TrainInput")
-      with tf.variable_scope("Model", reuse=None, initializer=initializer):
-        m = PTBModel(is_training=True, config=config, input_=train_input)
-      tf.summary.scalar("Training Loss", m.cost)
-      tf.summary.scalar("Learning Rate", m.lr)
+    with tf.Graph().as_default():
+      initializer = tf.random_uniform_initializer(-config.init_scale,
+                                                  config.init_scale)
 
-    with tf.name_scope("Valid"):
-      valid_input = PTBInput(config=config, data=valid_data, name="ValidInput")
-      with tf.variable_scope("Model", reuse=True, initializer=initializer):
-        mvalid = PTBModel(is_training=False, config=config, input_=valid_input)
-      tf.summary.scalar("Validation Loss", mvalid.cost)
+      with tf.name_scope("Train"):
+        train_input = PTBInput(config=config, data=train_data, name="TrainInput")
+        with tf.variable_scope("Model", reuse=None, initializer=initializer):
+          m = PTBModel(is_training=True, config=config, input_=train_input)
+        tf.summary.scalar("Training Loss", m.cost)
+        tf.summary.scalar("Learning Rate", m.lr)
 
-    with tf.name_scope("Test"):
-      test_input = PTBInput(
-          config=eval_config, data=test_data, name="TestInput")
-      with tf.variable_scope("Model", reuse=True, initializer=initializer):
-        mtest = PTBModel(is_training=False, config=eval_config,
-                         input_=test_input)
+      with tf.name_scope("Valid"):
+        valid_input = PTBInput(config=config, data=valid_data, name="ValidInput")
+        with tf.variable_scope("Model", reuse=True, initializer=initializer):
+          mvalid = PTBModel(is_training=False, config=config, input_=valid_input)
+        tf.summary.scalar("Validation Loss", mvalid.cost)
 
-    models = {"Train": m, "Valid": mvalid, "Test": mtest}
-    for name, model in models.items():
-      model.export_ops(name)
-    metagraph = tf.train.export_meta_graph()
-    if tf.__version__ < "1.1.0" and FLAGS.num_gpus > 1:
-      raise ValueError("num_gpus > 1 is not supported for TensorFlow versions "
-                       "below 1.1.0")
-    soft_placement = False
-    if FLAGS.num_gpus > 1:
-      soft_placement = True
-      util.auto_parallel(metagraph, m)
+      with tf.name_scope("Test"):
+        test_input = PTBInput(
+            config=eval_config, data=test_data, name="TestInput")
+        with tf.variable_scope("Model", reuse=True, initializer=initializer):
+          mtest = PTBModel(is_training=False, config=eval_config,
+                           input_=test_input)
 
-  with tf.Graph().as_default():
-    tf.train.import_meta_graph(metagraph)
-    for model in models.values():
-      model.import_ops()
-    sv = tf.train.Supervisor(logdir=FLAGS.save_path)
-    config_proto = tf.ConfigProto(allow_soft_placement=soft_placement)
-    with sv.managed_session(config=config_proto) as session:
-      for i in range(config.max_max_epoch):
-        lr_decay = config.lr_decay ** max(i + 1 - config.max_epoch, 0.0)
-        m.assign_lr(session, config.learning_rate * lr_decay)
+      models = {"Train": m, "Valid": mvalid, "Test": mtest}
+      for name, model in models.items():
+        model.export_ops(name)
+      metagraph = tf.train.export_meta_graph()
 
-        print("Epoch: %d Learning rate: %.3f" % (i + 1, session.run(m.lr)))
-        train_perplexity = run_epoch(session, m, eval_op=m.train_op,
-                                     verbose=True)
-        print("Epoch: %d Train Perplexity: %.3f" % (i + 1, train_perplexity))
-        valid_perplexity = run_epoch(session, mvalid)
-        print("Epoch: %d Valid Perplexity: %.3f" % (i + 1, valid_perplexity))
+      soft_placement = False
+ 
 
-      test_perplexity = run_epoch(session, mtest)
-      print("Test Perplexity: %.3f" % test_perplexity)
+    with tf.Graph().as_default():
+      tf.train.import_meta_graph(metagraph)
+      for model in models.values():
+        model.import_ops()
+      sv = tf.train.Supervisor(logdir=FLAGS.save_path)
+      config_proto = tf.ConfigProto(allow_soft_placement=soft_placement)
+      with sv.managed_session(config=config_proto) as session:
+        for i in range(config.max_max_epoch):
+          lr_decay = config.lr_decay ** max(i + 1 - config.max_epoch, 0.0)
+          m.assign_lr(session, config.learning_rate * lr_decay)
 
-      if FLAGS.save_path:
-        print("Saving model to %s." % FLAGS.save_path)
-        sv.saver.save(session, FLAGS.save_path, global_step=sv.global_step)
+          print("Epoch: %d Learning rate: %.3f" % (i + 1, session.run(m.lr)))
+          train_perplexity = run_epoch(session, m, eval_op=m.train_op,
+                                       verbose=True)
+          print("Epoch: %d Train Perplexity: %.3f" % (i + 1, train_perplexity))
+          valid_perplexity = run_epoch(session, mvalid)
+          print("Epoch: %d Valid Perplexity: %.3f" % (i + 1, valid_perplexity))
+
+        test_perplexity = run_epoch(session, mtest)
+        print("Test Perplexity: %.3f" % test_perplexity)
+
+        if FLAGS.save_path:
+          print("Saving model to %s." % FLAGS.save_path)
+          sv.saver.save(session, FLAGS.save_path, global_step=sv.global_step)
 
 
 if __name__ == "__main__":
